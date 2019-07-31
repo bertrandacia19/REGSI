e6fded72511a403a7a739c34a460f2969fddb7ad
Josue Lopez <lopez.josue201590@gmail.com>
2019-07-31 17:55:08 -0600

    Commit de Josue

---
+++ REGSI/Menu.vb
@@ -0,0 +1,7 @@
+﻿Imports WindowsApplication1
+Public Class Menu
+    Dim Solvencia As New WindowsApplication1.Form1
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Solvencia.ShowDialog()
+    End Sub
+End Class
